06d634114017797daea4a725218efe3277514b40
Thomas Findelkind <thomas@thomas-findelkind.de>
2019-03-22 12:22:02 +0100

    working with componentMount

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const os = require("os");
 const app = express();
-const gpio = require('rpi-gpio');
+const Gpio = require('onoff').Gpio;
 
-gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(11, gpio.DIR_OUT);
+const gpio4 = new Gpio(4, 'out');
+const gpio17 = new Gpio(17, 'out');
+
+var gpio4_state=gpio4.readSync();
+var gpio17_state=gpio17.readSync();
 
 app.use(express.static("dist"));
 
@@ -20,32 +23,38 @@ app.use((req, res, next) => {
   res.send("API 1.0 of gpio-ui")
 );
 
-app.post("/api/gpio7/on",function(req,res){
-  gpio.write(7, true, function(err) {
-        if (err) throw err;
-    });
-  res.end("GPIO 7 ON");
+app.get("/api/gpio4/state", (req, res) => {
+  gpio4_state=gpio4.readSync();
+  res.send(gpio4_state ? "1" : "0");
+ }
+);
+
+
+
+app.get("/api/gpio17/state", (req, res) => {
+  gpio17_state=gpio17.readSync();
+  res.send(gpio17_state ? "1" : "0");
+ }
+);
+
+app.post("/api/gpio4/on",function(req,res){
+  gpio4.writeSync(1);
+  res.end("GPIO 4 ON");
 });
 
-app.post("/api/gpio7/off",function(req,res){
-  gpio.write(7, false, function(err) {
-        if (err) throw err;
-    });
-  res.end("GPIO 7 OFF");
+app.post("/api/gpio4/off",function(req,res){
+  gpio4.writeSync(0);
+  res.end("GPIO 4 OFF");
 });
 
-app.post("/api/gpio11/on",function(req,res){
-  gpio.write(11, true, function(err) {
-        if (err) throw err;
-    });
-  res.end("GPIO 11 ON");
+app.post("/api/gpio17/on",function(req,res){
+  gpio17.writeSync(1);
+  res.end("GPIO 17 ON");
 });
 
-app.post("/api/gpio11/off",function(req,res){
-  gpio.write(11, false, function(err) {
-        if (err) throw err;
-    });
-  res.end("GPIO 11 OFF");
+app.post("/api/gpio17/off",function(req,res){
+  gpio17.writeSync(0);
+  res.end("GPIO 17 OFF");
 });
 
 
